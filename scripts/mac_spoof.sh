e0bdd15be2923b088669bf9830aa196bd5450a07
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-01 11:40:26 +0100

    Update mac_spoof.sh
    Made script more modular by getting prefix from old MAC address

--- scripts/mac_spoof.sh
+++ scripts/mac_spoof.sh
@@ -1,13 +1,17 @@
 #!/bin/bash                                                                                                                                                     
 function generate_mac ()  {
     hexchars="0123456789abcdef"
-    echo "60:57:18$(
+    echo "$prefix$(
         for i in {1..6}; do 
             echo -n ${hexchars:$(( $RANDOM % 16 )):1}
         done | sed -e 's/\(..\)/:\1/g'
     )"
 }
-MAC=$(generate_mac)
+old_mac=$(ip link show wlp4s0 | awk '/ether/ {print $2}')
+prefix=${old_mac:0:8}
+mac=$(generate_mac)
 ip link set dev wlp4s0 down
-ip link set dev wlp4s0 address $MAC
+ip link set dev wlp4s0 address $mac
 ip link set dev wlp4s0 up
+echo 'Old MAC address: ' ${old_mac}
+echo 'New MAC address: ' ${mac}
