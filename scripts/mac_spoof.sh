780755e1c2acbd0315a69e1ad5dd5d8a92bd4ba4
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-01 12:06:18 +0100

    Update mac_spoof.sh
    Runs every 59 minutes.

--- scripts/mac_spoof.sh
+++ scripts/mac_spoof.sh
@@ -1,17 +1,22 @@
-#!/bin/bash                                                                                                                                                     
-function generate_mac ()  {
-    hexchars="0123456789abcdef"
-    echo "$prefix$(
-        for i in {1..6}; do 
-            echo -n ${hexchars:$(( $RANDOM % 16 )):1}
-        done | sed -e 's/\(..\)/:\1/g'
-    )"
-}
-old_mac=$(ip link show wlp4s0 | awk '/ether/ {print $2}')
-prefix=${old_mac:0:8}
-mac=$(generate_mac)
-ip link set dev wlp4s0 down
-ip link set dev wlp4s0 address $mac
-ip link set dev wlp4s0 up
-echo 'Old MAC address: ' ${old_mac}
-echo 'New MAC address: ' ${mac}
+#!/bin/bash
+while true
+do
+    function generate_mac ()  {
+        hexchars="0123456789abcdef"
+        echo "$prefix$(
+            for i in {1..6}; do 
+                echo -n ${hexchars:$(( $RANDOM % 16 )):1}
+            done | sed -e 's/\(..\)/:\1/g'
+        )"
+    }
+    old_mac=$(ip link show wlp4s0 | awk '/ether/ {print $2}')
+    prefix=${old_mac:0:8}
+    MAC=$(generate_mac)
+    ip link set dev wlp4s0 down
+    ip link set dev wlp4s0 address $MAC
+    ip link set dev wlp4s0 up
+    echo 'Old MAC address: ' ${old_mac}
+    echo 'New MAC address: ' ${MAC}
+    sleep 3540                                                                                                                                                  
+    wait
+done
